ead3ddd647ece2528958aa495d82ca4e6e15fa3f
emfglab <emfglab@gmail.com>
2015-04-05 23:17:22 +0900

    initial commit for easy c# wrapper of openxml 2.5 sdk

---
+++ OpenXML25/OpenXML25/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyOXML;
+
+namespace OpenXML25
+{
+    public partial class Form1 : Form
+    {
+        XMLManager eoxml = new XMLManager();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.ShowDialog();
+                if (dlg.FileName != "") eoxml.CreatePackage(dlg.FileName);
+            }
+
+        }
+        
+    }
+    
+}
